6eebe67aca9636b0b15b521d6cccab938bb324a8
TXHENG <TXHENG@users.noreply.github.com>
2021-08-04 09:35:40 +0800

    design update

--- ArtGallery/Artist/main.Master.designer.cs
+++ ArtGallery/Artist/main.Master.designer.cs
@@ -33,6 +33,15 @@ namespace ArtGallery.Artist
         protected global::System.Web.UI.WebControls.ContentPlaceHolder MasterTitle;
 
         /// <summary>
+        /// form control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form;
+
+        /// <summary>
         /// Content control.
         /// </summary>
         /// <remarks>
